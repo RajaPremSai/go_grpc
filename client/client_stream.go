5e2c69228bbc8d787f79092db2418f9b31eb6a8f
rajapremsai <premsaimaddula342@gmail.com>
2025-08-06 22:38:52 +0530

    Completed Client Streaming

---
+++ client/client_stream.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/rajaprmesai/go_grpc/proto"
+)
+
+func callSayHelloClientStream(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Printf("Client Streaming started")
+
+	stream, err := client.SayHelloClientSideStreaming(context.Background())
+
+	if err != nil {
+		log.Fatalf("could not send names:%v", err)
+	}
+
+	for _, name := range names.Names {
+		req := &pb.HelloRequest{
+			Message: name,
+		}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("Error while sending %v", err)
+		}
+		log.Printf("Send the request with name : %s", name)
+		time.Sleep(2 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	log.Printf("Client Streaming finished")
+	if err != nil {
+		log.Fatalf("Error while recieving %v", err)
+	}
+
+	log.Printf("%v", res.Messages)
+}
---
+++ server/client_stream.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/rajaprmesai/go_grpc/proto"
+)
+
+func (s *helloServer) SayHelloClientSideStreaming(stream pb.GreetService_SayHelloClientSideStreamingServer) error {
+	var messages []string
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.MessageList{Messages: messages})
+		}
+
+		if err != nil {
+			return err
+		}
+		log.Printf("Got request with name  %v", req.Message)
+
+		messages = append(messages, "Hello "+req.Message)
+	}
+
+}
